d898ded52e3b0dd0b46ff0024ea32a4979e4e475
Leesin0222 <dragon02229@naver.com>
2022-09-05 19:17:11 +0900

    ⚡ :: MakeHomeActivity 작성

---
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/room/MakeHomeActivity.kt
@@ -0,0 +1,19 @@
+package com.yongjincompany.voyager.ui.room
+
+import android.os.Bundle
+import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.databinding.ActivityMakeHomeBinding
+import com.yongjincompany.voyager.ui.base.BaseActivity
+
+class MakeHomeActivity: BaseActivity<ActivityMakeHomeBinding>(
+    R.layout.activity_make_home
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initView() {
+
+    }
+
+}
