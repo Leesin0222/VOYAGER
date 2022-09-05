94a076f3016f71286e857db3b64dab7fabbb3e61
Leesin0222 <dragon02229@naver.com>
2022-09-06 01:22:24 +0900

    ♻️ :: FinishRocketBinding import로 간략화

--- voyager/app/src/main/java/com/yongjincompany/voyager/ui/room/FinishRocketActivity.kt
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/room/FinishRocketActivity.kt
@@ -3,10 +3,11 @@ package com.yongjincompany.voyager.ui.room
 import android.content.Intent
 import android.os.Bundle
 import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.databinding.ActivityFinishRocketBinding
 import com.yongjincompany.voyager.ui.base.BaseActivity
 import com.yongjincompany.voyager.ui.home.MainActivity
 
-class FinishRocketActivity: BaseActivity<com.yongjincompany.voyager.databinding.ActivityFinishRocketBinding>(
+class FinishRocketActivity: BaseActivity<ActivityFinishRocketBinding>(
     R.layout.activity_finish_rocket
 ) {
     override fun onCreate(savedInstanceState: Bundle?) {
