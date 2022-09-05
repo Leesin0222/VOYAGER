43550658eaf8baf7fb7a2da24c4baba51aefff11
Leesin0222 <dragon02229@naver.com>
2022-09-06 00:09:48 +0900

    ⚡ :: MakeRocketActivity 생성

---
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/room/MakeRocketActivity.kt
@@ -0,0 +1,17 @@
+package com.yongjincompany.voyager.ui.room
+
+import android.os.Bundle
+import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.databinding.ActivityMakeRocketBinding
+import com.yongjincompany.voyager.ui.base.BaseActivity
+
+class MakeRocketActivity : BaseActivity<ActivityMakeRocketBinding>(
+    R.layout.activity_make_rocket
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun initView() {
+
+    }
+}
