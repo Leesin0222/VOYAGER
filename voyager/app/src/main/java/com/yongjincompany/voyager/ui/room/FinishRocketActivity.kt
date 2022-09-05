4d8a3d7c218972e32c47a0cb6cc8b51826763753
Leesin0222 <dragon02229@naver.com>
2022-09-06 01:20:08 +0900

    ⚡ :: 만들기 완료 버튼 누를시 홈으로 이동하는 코드 작성

---
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/room/FinishRocketActivity.kt
@@ -0,0 +1,21 @@
+package com.yongjincompany.voyager.ui.room
+
+import android.content.Intent
+import android.os.Bundle
+import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.ui.base.BaseActivity
+import com.yongjincompany.voyager.ui.home.MainActivity
+
+class FinishRocketActivity: BaseActivity<com.yongjincompany.voyager.databinding.ActivityFinishRocketBinding>(
+    R.layout.activity_finish_rocket
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun initView() {
+        binding.btnFinish.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
