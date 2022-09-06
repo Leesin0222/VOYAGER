b9a56f6f340560b4a99ace7503f50fb14db01227
Leesin0222 <dragon02229@naver.com>
2022-09-06 17:34:41 +0900

    ⚡ :: BoardHomeActivity로 넘어가는 코드 작성

--- voyager/app/src/main/java/com/yongjincompany/voyager/ui/home/MainActivity.kt
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/home/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.yongjincompany.voyager.R
 import com.yongjincompany.voyager.databinding.ActivityMainBinding
 import com.yongjincompany.voyager.ui.base.BaseActivity
+import com.yongjincompany.voyager.ui.participate.BoardHomeActivity
 import com.yongjincompany.voyager.ui.room.MakeHomeActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
@@ -18,6 +19,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
     override fun initView() {
         binding.btnMakeRocket.setOnClickListener {
             val intent = Intent(this, MakeHomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnBoardRocket.setOnClickListener {
+            val intent = Intent(this, BoardHomeActivity::class.java)
             startActivity(intent)
         }
     }
