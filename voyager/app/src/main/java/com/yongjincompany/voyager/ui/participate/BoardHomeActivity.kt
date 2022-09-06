5fb4371f4be9d25c265f248c54928c804ecaeb2f
Leesin0222 <dragon02229@naver.com>
2022-09-06 17:34:18 +0900

    ⚡ :: 로켓 타기 버튼 누를 시 대기자 목록 보여주는 페이지로 이동하는 코드 작성

---
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/participate/BoardHomeActivity.kt
@@ -0,0 +1,21 @@
+package com.yongjincompany.voyager.ui.participate
+
+import android.content.Intent
+import android.os.Bundle
+import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.databinding.ActivityBoardHomeBinding
+import com.yongjincompany.voyager.ui.base.BaseActivity
+
+class BoardHomeActivity : BaseActivity<ActivityBoardHomeBinding>(
+    R.layout.activity_board_home
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun initView() {
+        binding.btnBoardRocket.setOnClickListener {
+            val intent = Intent(this, BoardWaitingActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
