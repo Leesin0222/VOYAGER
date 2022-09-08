7e3724c561dd3365b986331d17d5b542425ce265
Leesin0222 <dragon02229@naver.com>
2022-09-08 21:14:45 +0900

    ⚡ :: ResultActivity 작성

---
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/end/ResultActivity.kt
@@ -0,0 +1,20 @@
+package com.yongjincompany.voyager.ui.end
+
+import android.os.Bundle
+import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.databinding.ActivityResultBinding
+import com.yongjincompany.voyager.ui.base.BaseActivity
+
+class ResultActivity : BaseActivity<ActivityResultBinding>(
+    R.layout.activity_result
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun initView() {
+
+    }
+
+}
