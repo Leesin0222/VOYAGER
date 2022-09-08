374026fecc1fad27b5fb89035f67d197db350c57
Leesin0222 <dragon02229@naver.com>
2022-09-09 04:04:07 +0900

    ⚡ :: MainActivity로 넘어가는 코드 작성

---
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/login/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.yongjincompany.voyager.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import com.yongjincompany.voyager.R
+import com.yongjincompany.voyager.databinding.ActivityLoginBinding
+import com.yongjincompany.voyager.ui.base.BaseActivity
+import com.yongjincompany.voyager.ui.home.MainActivity
+
+class LoginActivity:BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun initView() {
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
+
+
