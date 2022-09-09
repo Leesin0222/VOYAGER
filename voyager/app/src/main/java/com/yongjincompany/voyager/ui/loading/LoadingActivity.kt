9d662e128b36ca6ad8ac50da54e73036925cfdfa
Leesin0222 <dragon02229@naver.com>
2022-09-09 16:10:30 +0900

    ⚡ :: 로딩 화면 클릭 시 메모Activity로 넘어가는 코드 구현

--- voyager/app/src/main/java/com/yongjincompany/voyager/ui/loading/LoadingActivity.kt
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/loading/LoadingActivity.kt
@@ -1,10 +1,16 @@
 package com.yongjincompany.voyager.ui.loading
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.bumptech.glide.Glide
 import com.yongjincompany.voyager.R
 import com.yongjincompany.voyager.databinding.ActivityLoadingBinding
 import com.yongjincompany.voyager.ui.base.BaseActivity
+import com.yongjincompany.voyager.ui.login.HomeActivity
+import com.yongjincompany.voyager.ui.memo.MemoActivity
 
 class LoadingActivity: BaseActivity<ActivityLoadingBinding>(
     R.layout.activity_loading
@@ -16,5 +22,10 @@ class LoadingActivity: BaseActivity<ActivityLoadingBinding>(
     override fun initView() {
         //TODO: 달이 막 화면으로 점점 커져서 도착한 것 같은 효과를 내게 하자 어차피 로딜화면 아니우주로 가는 화면이니깐..!
         Glide.with(this).load(R.drawable.space).centerCrop().into(binding.ivSpaceBackground)
+
+        binding.all.setOnClickListener {
+            val intent = Intent(this, MemoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
