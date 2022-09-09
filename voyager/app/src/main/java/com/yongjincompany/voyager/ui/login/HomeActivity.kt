92ce8aebd73b5a7a5bd5c400063728a8f6244b15
Leesin0222 <dragon02229@naver.com>
2022-09-09 11:29:16 +0900

    ⚡ :: 화면이 넘어가는 애니메이션 방식 변경

--- voyager/app/src/main/java/com/yongjincompany/voyager/ui/login/HomeActivity.kt
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/login/HomeActivity.kt
@@ -6,23 +6,26 @@ import com.yongjincompany.voyager.R
 import com.yongjincompany.voyager.databinding.ActivityHomeBinding
 import com.yongjincompany.voyager.ui.base.BaseActivity
 
-class HomeActivity: BaseActivity<ActivityHomeBinding>(
+class HomeActivity : BaseActivity<ActivityHomeBinding>(
     R.layout.activity_home
 ) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
+
     override fun initView() {
         binding.tvLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            overridePendingTransition(com.google.android.material.R.anim.abc_slide_in_bottom,
+                com.google.android.material.R.anim.abc_slide_in_top)
         }
         binding.btnJoin.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
-            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            overridePendingTransition(com.google.android.material.R.anim.abc_slide_in_bottom,
+                com.google.android.material.R.anim.abc_slide_in_top)
         }
     }
 }
