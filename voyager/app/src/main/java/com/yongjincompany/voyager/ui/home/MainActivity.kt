28cbb909b6bfa34dbf0e853891145066eec99204
Leesin0222 <dragon02229@naver.com>
2022-09-09 16:12:21 +0900

    ⚡ :: rocketList에 더미값 넣기 및 adapter, layoutmanager 설정

--- voyager/app/src/main/java/com/yongjincompany/voyager/ui/home/MainActivity.kt
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/home/MainActivity.kt
@@ -3,9 +3,12 @@ package com.yongjincompany.voyager.ui.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yongjincompany.voyager.R
 import com.yongjincompany.voyager.databinding.ActivityMainBinding
+import com.yongjincompany.voyager.remote.response.Rocket
 import com.yongjincompany.voyager.ui.base.BaseActivity
+import com.yongjincompany.voyager.ui.home.adapter.RocketListAdapter
 import com.yongjincompany.voyager.ui.loading.LoadingActivity
 import com.yongjincompany.voyager.ui.memo.MemoActivity
 import com.yongjincompany.voyager.ui.participate.BoardHomeActivity
@@ -16,6 +19,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 ) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var rocketList = mutableListOf(
+            Rocket("유니톤9호", "탑승대기"),
+            Rocket("드래곤호", "탑승완료"),
+            Rocket("나로호", "항해종료"),
+            Rocket("디자인짱호", "항해중")
+        )
+        val myAdapter by lazy {
+            RocketListAdapter(rocketList)
+        }
+
+        binding.rvRocketList.adapter = myAdapter
+        binding.rvRocketList.layoutManager = LinearLayoutManager(this)
     }
 
     override fun initView() {
@@ -29,15 +45,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
             val intent = Intent(this, BoardHomeActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
-        }
-
-        binding.textView4.setOnClickListener {
-            val intent = Intent(this, LoadingActivity::class.java)
-            startActivity(intent)
-        }
-        binding.textView3.setOnClickListener {
-            val intent = Intent(this, MemoActivity::class.java)
-            startActivity(intent)
         }
     }
 }
