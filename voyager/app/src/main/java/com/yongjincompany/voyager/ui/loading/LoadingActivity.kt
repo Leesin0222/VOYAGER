b3f158a5e3d2a6d98167735f249144a51a6026e7
Leesin0222 <dragon02229@naver.com>
2022-09-10 10:02:22 +0900

    ⚡ :: 달이 돌아가는 코드 작성

--- voyager/app/src/main/java/com/yongjincompany/voyager/ui/loading/LoadingActivity.kt
+++ voyager/app/src/main/java/com/yongjincompany/voyager/ui/loading/LoadingActivity.kt
@@ -1,5 +1,6 @@
 package com.yongjincompany.voyager.ui.loading
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -22,6 +23,12 @@ class LoadingActivity: BaseActivity<ActivityLoadingBinding>(
     override fun initView() {
         //TODO: 달이 막 화면으로 점점 커져서 도착한 것 같은 효과를 내게 하자 어차피 로딜화면 아니우주로 가는 화면이니깐..!
         Glide.with(this).load(R.drawable.space).centerCrop().into(binding.ivSpaceBackground)
+        ObjectAnimator.ofFloat(binding.ivMoon, View.ROTATION, 0f, 30000f)
+            .apply {
+                duration = 360000
+                repeatCount = Animation.INFINITE
+                start()
+            }
 
         binding.all.setOnClickListener {
             val intent = Intent(this, MemoActivity::class.java)
